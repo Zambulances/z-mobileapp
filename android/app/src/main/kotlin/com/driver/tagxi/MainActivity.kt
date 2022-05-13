3c478c8496879ad56bdb3a63733f836d93e1b06d
Mobility Intelligence Softwares <mobilityintelligencesoftwares@Mobilitys-MacBook-Pro.local>
2022-05-13 13:08:14 +0530

    13052022

---
+++ android/app/src/main/kotlin/com/driver/tagxi/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.driver.tagxi
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
